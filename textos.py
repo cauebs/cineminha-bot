74338f6dfe93d526ba0ebb34c8374079b89055f5
Cauê Baasch de Souza <cauebaasch@gmail.com>
2016-07-12 21:15:01 -0300

    Location Updating working

--- textos.py
+++ textos.py
@@ -1,4 +1,4 @@
 start_text = '''Olá! Envie sua localização para começar (ela não será compartilhada com ninguém). 
-Use o botão abaixo ou, se preferir, envie "/local endereço"'''
+Você também enviar /local endereço-ou-cidade'''
 local_atualizado_text = '''Localização atualizada!
-Você pode mudá-la a qualquer momento compartilhando uma localização ou enviando "/local seu-endereço"'''
+Você pode mudá-la a qualquer momento compartilhando uma localização ou enviando /local endereço-ou-cidade'''
