5165960a44f3d48bae1054ddd7a0b0c2fb1b79ce
Cauê Baasch de Souza <cauebaasch@gmail.com>
2016-07-21 17:01:05 -0300

    Another tiny little motherfucking bug

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ def cinemas(bot, update):
 	if loc is None:
 		bot.sendMessage(update.message.chat_id,text=local_nao_definido, parse_mode="Markdown")
 	else:
-		for i in fetch.cineminha(fetch.serialize(loc, detail=True)):
+		for i in fetch.cineminha(fetch.serialize(loc)):
 			bot.sendMessage(update.message.chat_id,text=i, parse_mode="Markdown")
 
 def pesquisar(bot, update):
